bd0bc2ca232ca7c456f876eb15d73ac1ecadf7e6
Svitlana Zhovanik <lanasvetcat@gmail.com>
2024-11-13 12:45:42 +0200

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.unsplash.com',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
